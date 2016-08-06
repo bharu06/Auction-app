9c9e1adb29f79729a3e29b9f51e0a93754a061ab
bharu06 <manusanibhargavi14@gmail.com>
2016-08-06 14:04:59 +0530

    Update settings.py

--- finalApp/settings.py
+++ finalApp/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': 'auctiondb',
         'USER': 'root',
         'PASSWORD': '1234',
-        'HOST':'localhost',
+        'HOST':'127.0.0.1',
         'PORT':'3306',
         'OPTIONS':{
             'autocommit':True
